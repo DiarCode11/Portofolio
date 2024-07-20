9e61885447f4a846e62da3aa4f564a6dd5d632fc
Diar Sudiar <163659290+DiarCode11@users.noreply.github.com>
2024-07-20 11:32:48 +0800

    update responsive 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
-      screens: {
-      
-      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        'phone': {'min': '240px', 'max': '479px'},
+        'tablet': {'min': '440px', 'max': '1023px'},
+        'desktop': {'min': '1024px', 'max': '1280px'},
       },
     },
   },
